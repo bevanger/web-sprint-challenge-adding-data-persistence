604cca306bf824fed1137655da050cb7c5655ab8
bevanger <baileyevanger@gmail.com>
2021-09-10 09:19:28 -0600

    added to package.json, index.js, server.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,19 @@
-// build your server here and require it from index.js
+const express = require('express');
+const projectRouter = require('./project/router');
+const actionRouter = require('./resource/router');
+const taskRouter = require('./task/router');
+const server = express();
+
+server.use(express.json());
+server.use('/api/project', projectRouter);
+server.use('/api/action', actionRouter);
+server.use('/api/task', taskRouter);
+
+server.use((err, req, res, next) => { 
+    res.status(err.status || 500).json({
+      message: err.message,
+      stack: err.stack,
+    })
+  })
+
+module.exports = server;
